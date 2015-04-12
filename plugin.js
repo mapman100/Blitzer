18bae81eacd1c0678b0b43ffe47ac8bb5e89bd39
mapman100 <g13722966@trbvm.com>
2015-04-12 11:57:13 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,27 @@
+{
+    "moduleId" : "mapman100/Blitzer",
+    "title" : "Blitzer.de",
+    "subtitle" : "www.blitzer.de",
+    
+    "backButton" : false,
+    "enableGPS" : false,
+    "zoomControl" : false,
+    "screenLockRot" : false,
+    "reload" : true,
+    
+    "shouldOverrideUrlLoading1" : "http://mm.blitzer.de",
+    "shouldOverrideUrlLoading2" : "https://mm.blitzer.de",
+
+    "loadUrl" : "http://mm.blitzer.de",
+					    			
+    "browserLaunchLink" : "http://mm.blitzer.de",
+    
+    "onPageFinishedLoadUrl" : "
+javascript:
+var myCoords = { latitude:#lat#, longitude:#lng#, altitude:0, accuracy:10, altitudeAccuracy:10, heading:0, speed:0 };
+var myPos = { coords:myCoords, timestamp: (new Date()) };
+write_cookie('zoomlevel', #zoom#);
+handle_position_query(myPos);
+foundGPS(1);
+    "
+}
