b1d8e65fa31c1b636343c08f638ed45d755bddb6
Juri Gagarov <mapman100@yandex.com>
2018-09-01 16:23:31 +0200

    Support ATUDO V3

--- plugin.js
+++ plugin.js
@@ -1,28 +1,56 @@
 {
-    "moduleId" : "mapman100/Blitzer",
-    "title" : "Blitzer.de",
-    "subtitle" : "www.blitzer.de",
-    
-    "backButton" : false,
-    "enableGPS" : false,
-    "zoomControl" : false,
-    "screenLockRot" : false,
-    "reload" : true,
-    
-    "shouldOverrideUrlLoading1" : "http://mm.blitzer.de",
-    "shouldOverrideUrlLoading2" : "https://mm.blitzer.de",
+  "moduleId" : "mapman100/Blitzer",
+  "title" : "Blitzer",
+  "subtitle" : "Blitzer from blitzer.de",
 
-    "loadUrl" : "http://mm.blitzer.de",
-					    			
-    "browserLaunchLink" : "http://mm.blitzer.de",
-    
-    "onPageFinishedLoadUrl" : "javascript:
-var myCoords = { latitude:#lat#, longitude:#lng#, altitude:0, accuracy:10, altitudeAccuracy:10, heading:0, speed:0 };
-var myPos = { coords:myCoords, timestamp: (new Date()) };
-write_cookie('zoomlevel', #zoom#);
-write_cookie('fest', 1);
-write_cookie('traffic', 1);
-handle_position_query(myPos);
-foundGPS(1);
-    "
+  "backButton" : false,
+  "enableGPS" : false,
+  "zoomControl" : false,
+  "screenLockRot" : false,
+  "reload" : false,
+
+  "shouldOverrideUrlLoading1" : "https://map.atudo.com",
+  "shouldOverrideUrlLoading2" : "http://map.atudo.com/",
+
+  "browserLaunchLink" : "https://map.atudo.com",
+  "userAgent" : "",
+
+  "loadDataWithBaseUrl1" : "https://map.atudo.com/v3/?geoloc=0",
+  "loadDataWithBaseUrl3" : "text/html",
+  "loadDataWithBaseUrl4" : "utf-8",
+  "loadDataWithBaseUrl5" : null,
+
+  "loadDataWithBaseUrl2" :
+  "
+  <!DOCTYPE html>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+  <link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css'>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js'></script>
+
+  <script type='text/javascript'>
+  window.loadPage = function (url) {
+  alert('start replacing ...');
+  $.get(url, function(page) {
+
+  newPage = page.replace('lat_global = 51.163361;', 'lat_global = #lat#');
+  newPage = newPage.replace('lng_global = 10.447683;', 'lng_global = #lng# ');
+
+  var newDoc = document.open();
+  newDoc.write(newPage);
+  newDoc.close();
+
+  alert(newPage);
+
+});
+};
+</script>
+
+<script>
+loadPage( 'https://map.atudo.com/v3/?geoloc=0');
+</script>
+",
+
+"onPageFinishedLoadUrl" : "javascript:
+setTimeout(function() { map.setZoom(#zoom#); }, 1000);"
+
 }
